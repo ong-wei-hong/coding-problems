70c103e1b6da3123d8c1dfaa72e3daeec544ed91
ong-wei-hong <weihong.ong@outlook.com>
2022-06-26 19:49:43 +0800

    edited kattis control

--- kattis/control/main.cpp
+++ kattis/control/main.cpp
@@ -9,44 +9,47 @@ using namespace std;
 #define all(a) a.begin(),a.end()
 #define rall(a) a.rbegin(),a,rend()
 
-int n,m,ufds[500001],s[500001];
+int n,m,a[5000001],ans,b[5000001];
 
 void solve() {
 }
 
-int find(int i) {
-	if(ufds[i]!=i) ufds[i]=find(ufds[i]);
-	return ufds[i];
-}
-
-void union_set(int i,int j) {
-	i=find(i),j=find(j);
-	if(i==j) return;
-	if(s[i]>s[j]) swap(i,j);
-	ufds[i]=j;
-	s[j]+=s[i];
-}
-
 int main() {
-	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
-	//freopen("","r",stdin);
-	//freopen("","w",stdout);
-	//cin>>t;
-	//while(t--) solve();
-	forn(i,500002) ufds[i]=i,s[i]=1;
-	cin>>n;
-	int ans{0};
-	while(n--) {
-		cin>>m;
-		vi v(m);
-		unordered_set<int> r{};
-		forn(i,m) cin>>v[i],r.insert(find(v[i]));
-		int t=0;
-		for(auto &i: r) t+=s[i];
-		if(t==m) {
-			++ans;
-			for(auto &i: v) union_set(v[0],i);
-		}
-	}
-	cout<<ans<<'\n';
+    ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+    //freopen("","r",stdin);
+    //freopen("","w",stdout);
+    //cin>>t;
+    //while(t--) solve();
+    cin>>n;
+    int c=1;
+    while(n--) {
+        cin>>m;
+        vi v(m);
+        forn(i,m) cin>>v[i];
+        unordered_map<int,int> map{};
+        forn(i,m) if(a[v[i]]) {
+            auto it=map.find(a[v[i]]);
+            if(it==map.end()) map[a[v[i]]]=1;
+            else ++(it->second);
+        }
+        if(map.empty()) {
+            ++ans;
+            forn(i,m) a[v[i]]=c;
+            b[c]=m;
+            ++c;
+            continue;
+        }
+        bool valid{true};
+        for(auto it=map.begin();it!=map.end();++it) 
+            if((it->second)!=b[it->first]) {
+                valid=false;
+                break;
+            }
+        if(!valid) continue;
+        ++ans;
+        int d=map.begin()->first;
+        forn(i,m) a[v[i]]=d;
+        b[d]=m;
+    }
+    cout<<ans<<'\n';
 }
