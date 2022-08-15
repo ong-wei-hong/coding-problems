5236a051593429a596831b3086b9cd057bf3eea9
ong-wei-hong <weihong.ong@outlook.com>
2022-08-15 08:24:54 +0800

    completed leetcode 704

---
+++ leetcode/704/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+		int search(vector<int>& nums,int target) {
+			int l=0,r=nums.size()-1;
+			while(l<=r) {
+				int m=(l+r)/2;
+				if(nums[m]==target) return m;
+				if(nums[m]<target) l=m+1;
+				else r=m-1;
+			}
+			return -1;
+		}
+	// time: O(log n) where n = nums.size()
+	// space: O(1)
+};
