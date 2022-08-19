6ca512e500555fe82f91c82947b6735dc4d72c93
ong-wei-hong <weihong.ong@outlook.com>
2022-08-19 10:28:32 +0800

    completed codeforces 1720 A

---
+++ codeforces/1720/A/main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define vii vector<pair<int, int>>
+#define pq priority_queue
+
+#define forn(i, n) for(int i=0; i<int(n); ++i)
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+int t;
+ll a,b,c,d;
+
+void solve();
+
+void file() {
+	freopen("","r",stdin);
+	freopen("","w",stdout);
+}
+
+void cin_ignore() {
+	cin.ignore(256,'\n');
+}
+
+int main() {
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	cin>>t;
+	while(t--) {
+		cin>>a>>b>>c>>d;
+		ll x=a*d,y=b*c;
+		if(x==y) cout<<0;
+		else if((y!=0&&x%y==0)||(x!=0&&y%x==0)) cout<<1;
+		else cout<<2;
+		cout<<'\n';
+	}
+}
