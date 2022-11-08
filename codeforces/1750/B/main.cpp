6e96cae547493eb9fec1dbaea4ca1e6aeac2bcb4
ong-wei-hong <weihong.ong@outlook.com>
2022-11-08 08:37:05 +0800

    completed codeforces 1750 B and online judge 1585

---
+++ codeforces/1750/B/main.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define vii vector<pair<int, int>>
+#define pq priority_queue
+
+#define forn(i, n) for(int i=0; i<int(n); ++i)
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+const ll MOD = 998244353;
+
+void file() {
+	freopen("","r",stdin);
+	freopen("","w",stdout);
+}
+
+void cin_ignore() {
+	cin.ignore(256,'\n');
+}
+
+int ufds_find_set(vi& ufds, int i){
+	if(ufds[i]==i) return i;
+	return ufds[i]=ufds_find_set(ufds,ufds[i]);
+}
+
+void ufds_union_set(vi& ufds, vi& rank, int i, int j){
+	i = ufds_find_set(ufds,i);
+	j = ufds_find_set(ufds,j);
+	if(i==j) return;
+	if(rank[i]>rank[j]) ufds[j]=i;
+	else ufds[i]=j,++rank[j];
+}
+
+ll add(ll x, ll y) {
+	x+=y;
+	while(x>=MOD) x-=MOD;
+	while(x<0) x+=MOD;
+	return x;
+}
+
+ll sub(ll x, ll y) {
+	return add(x, -y);
+}
+
+ll mul(ll x, ll y) {
+	return (x*y)%MOD;
+}
+
+ll binpow(ll x, ll y) {
+	ll z=1;
+	while(y){
+		if(y&1) z*=x;
+		x=mul(x,x);
+		y>>=1;
+	}
+	return z;
+}
+
+bool prime(ll x) {
+	for(ll i=2;i*i<=x;++i)
+		if(x%i==0) return false;
+	return true;
+}
+
+void solve();
+
+int t,n;
+string s;
+
+int main() {
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	cin>>t;
+	while(t--){
+		cin>>n>>s;
+		ll x,y,ans;
+		x=y=0;
+		for(auto i: s) x+=(i=='0'),y+=(i=='1');
+		ans=x*y;
+		ll curr,maxi;
+		curr=1,maxi=1;
+		forn(i,s.size()-1) {
+			curr=(s[i]==s[i+1])?(curr+1):1;
+			maxi=max(maxi,curr);
+		}
+		ans=max(ans,maxi*maxi);
+		cout<<ans<<'\n';
+	}
+}
