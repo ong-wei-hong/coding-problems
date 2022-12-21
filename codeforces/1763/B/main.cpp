09286dd599445bc6688a0213ecec72254bea91a8
ong-wei-hong <weihong.ong@outlook.com>
2022-12-21 10:26:59 +0800

    completed codeforces 1763 A B C

---
+++ codeforces/1763/B/main.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define vii vector<pair<int, int>>
+#define pq priority_queue
+
+#define forn(i, n) for(int i=0; i<int(n); ++i)
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+const ll MOD = 998244353;
+
+void file() {
+	freopen("","r",stdin);
+	freopen("","w",stdout);
+}
+
+void cin_ignore() {
+	cin.ignore(256,'\n');
+}
+
+struct ufds {
+	vi v,rank;
+	void init(int n) {
+		v.resize(n);
+		forn(i,n) v[i]=i;
+		rank.resize(n);
+		forn(i,n) rank[i]=1;
+	}
+	int find(int i) {
+		if(v[i]==i) return i;
+		return v[i]=this->find(v[i]);
+	}
+	void union_set(int i, int j){
+		i=this->find(i);
+		j=this->find(j);
+		if(i==j) return;
+		if(rank[i]>rank[j]) v[j]=i;
+		else v[i]=j,++rank[j];
+	}
+};
+
+ll add(ll x, ll y) {
+	x+=y;
+	while(x>=MOD) x-=MOD;
+	while(x<0) x+=MOD;
+	return x;
+}
+
+ll sub(ll x, ll y) {
+	return add(x, -y);
+}
+
+ll mul(ll x, ll y) {
+	return (x*y)%MOD;
+}
+
+ll binpow(ll x, ll y) {
+	ll z=1;
+	while(y){
+		if(y&1) z*=x;
+		x=mul(x,x);
+		y>>=1;
+	}
+	return z;
+}
+
+bool prime(ll x) {
+	for(ll i=2;i*i<=x;++i)
+		if(x%i==0) return false;
+	return true;
+}
+
+int t,n,k;
+vii v;
+
+void solve() {
+	cin>>n>>k;
+	v.resize(n);
+	for(auto& i: v) cin>>i.se;
+	for(auto& i: v) cin>>i.fi;
+	sort(all(v));
+	ll total=k;
+	int i=0;
+	while(1) {
+		while(i<n&&total>=v[i].se) ++i;
+		if(k<=0||i==n) break;
+		while(k>0&&total<v[i].se) k-=v[i].fi,total+=k;
+	}
+	if(i==n) cout<<"YES\n";
+	else cout<<"NO\n";
+}
+
+int main() {
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	cin>>t;
+	while(t--) solve();
+}
