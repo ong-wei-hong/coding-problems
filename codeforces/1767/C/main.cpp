d547e087089dd334d04d9170e764c32f524c1810
ong-wei-hong <weihong.ong@outlook.com>
2022-12-22 11:30:17 +0800

    completed codeforces 1767 C

---
+++ codeforces/1767/C/main.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define vii vector<pair<int, int>>
+#define pq priority_queue
+
+#define forn(i, n) for(int i=0; i<int(n); ++i)
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+const ll MOD = 998244353;
+
+void file() {
+	freopen("","r",stdin);
+	freopen("","w",stdout);
+}
+
+void cin_ignore() {
+	cin.ignore(256,'\n');
+}
+
+struct ufds {
+	vi v,rank;
+	void init(int n) {
+		v.resize(n);
+		forn(i,n) v[i]=i;
+		rank.resize(n);
+		forn(i,n) rank[i]=1;
+	}
+	int find(int i) {
+		if(v[i]==i) return i;
+		return v[i]=this->find(v[i]);
+	}
+	void union_set(int i, int j){
+		i=this->find(i);
+		j=this->find(j);
+		if(i==j) return;
+		if(rank[i]>rank[j]) v[j]=i;
+		else v[i]=j,++rank[j];
+	}
+};
+
+ll add(ll x, ll y) {
+	x+=y;
+	while(x>=MOD) x-=MOD;
+	while(x<0) x+=MOD;
+	return x;
+}
+
+ll sub(ll x, ll y) {
+	return add(x, -y);
+}
+
+ll mul(ll x, ll y) {
+	return (x*y)%MOD;
+}
+
+ll binpow(ll x, ll y) {
+	ll z=1;
+	while(y){
+		if(y&1) z*=x;
+		x=mul(x,x);
+		y>>=1;
+	}
+	return z;
+}
+
+bool prime(ll x) {
+	for(ll i=2;i*i<=x;++i)
+		if(x%i==0) return false;
+	return true;
+}
+
+int n;
+ll a[101][101],dp[101][101];
+
+void solve();
+
+bool check(int l, int last) {
+	forn(i,l) {
+		if(a[i][l-1]==1 && last>i) return false;
+		if(a[i][l-1]==2 && last<=i) return false;
+	}
+	return true;
+}
+
+int main() {
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	cin>>n;
+	forn(i,n) for(int j=i;j<n;++j) cin>>a[i][j];
+	if(a[0][0]!=2) dp[1][0]=2;
+	for(int i=1;i<n;++i) for(int j=0;j<i;++j) {
+		if(check(i+1,j)) dp[i+1][j] = add(dp[i+1][j], dp[i][j]);
+		if(check(i+1,i)) dp[i+1][i] = add(dp[i+1][i], dp[i][j]);
+	}
+	ll ans=0;
+	forn(i,n) ans = add(ans, dp[n][i]);
+	cout<<ans<<'\n';
+}
