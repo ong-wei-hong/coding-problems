eef88a1c4abc7abb1afea732926ff6075245c267
ong-wei-hong <weihong.ong@outlook.com>
2023-06-23 16:52:52 +0800

    completed online judge 584 and 10813

---
+++ online-judge/584/main.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define vii vector<pair<int, int>>
+#define pq priority_queue
+
+#define forn(i, n) for(int i=0; i<int(n); ++i)
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+const ll MOD = 998244353;
+
+void file() {
+	freopen("","r",stdin);
+	freopen("","w",stdout);
+}
+
+void cin_ignore() {
+	cin.ignore(256,'\n');
+}
+
+struct ufds {
+	vi v,rank;
+	void init(int n) {
+		v.resize(n);
+		forn(i,n) v[i]=i;
+		rank.resize(n);
+		forn(i,n) rank[i]=1;
+	}
+	int find(int i) {
+		if(v[i]==i) return i;
+		return v[i]=this->find(v[i]);
+	}
+	void union_set(int i, int j){
+		i=this->find(i);
+		j=this->find(j);
+		if(i==j) return;
+		if(rank[i]>rank[j]) v[j]=i;
+		else v[i]=j,++rank[j];
+	}
+};
+
+ll add(ll x, ll y) {
+	x+=y;
+	while(x>=MOD) x-=MOD;
+	while(x<0) x+=MOD;
+	return x;
+}
+
+ll sub(ll x, ll y) {
+	return add(x, -y);
+}
+
+ll mul(ll x, ll y) {
+	return (x*y)%MOD;
+}
+
+ll binpow(ll x, ll y) {
+	ll z=1;
+	while(y){
+		if(y&1) z=mul(z,x);
+		x=mul(x,x);
+		y>>=1;
+	}
+	return z;
+}
+
+ll inv(ll x) {
+	return binpow(x, MOD - 2) ;
+}
+
+bool prime(ll x) {
+	for(ll i=2;i*i<=x;++i)
+		if(x%i==0) return false;
+	return true;
+}
+
+int t,p;
+string line;
+
+void solve() {
+}
+
+int toInt(char c) {
+	if(c=='X')return 10;
+	if(c=='/') return 10-p;
+	p=(c-'0');
+	return p;
+}
+
+int main() {
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	while(getline(cin,line)) {
+		if(line=="Game Over") break;
+		int ans=0;
+		for(int frame=0,i=0;frame<10;++frame,i+=2) {
+			ans+=toInt(line[i]);
+			if(line[i]=='X') ans+=toInt(line[i+2])+toInt(line[i+4]);
+			else {
+				i+=2;
+				ans+=toInt(line[i]);
+				if(line[i]=='/') ans+=toInt(line[i+2]);
+			}
+		}
+		cout<<ans<<'\n';
+	}
+}
