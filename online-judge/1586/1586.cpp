7ee9ccd6756feba4dba5e7dc631721d30a667145
ong-wei-hong <weihong.ong@outlook.com>
2023-06-30 20:16:08 +0800

    completed kattis wertyu and online judge 1586 637

---
+++ online-judge/1586/1586.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ii pair<int, int>
+#define vi vector<int>
+#define vll vector<long long>
+#define vii vector<pair<int, int>>
+#define pq priority_queue
+
+#define forn(i, n) for(int i=0; i<int(n); ++i)
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+
+#define fi first
+#define se second
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+
+const ll MOD = 998244353;
+
+void file() {
+	freopen("","r",stdin);
+	freopen("","w",stdout);
+}
+
+void cin_ignore() {
+	cin.ignore(256,'\n');
+}
+
+struct ufds {
+	vi v,rank;
+	void init(int n) {
+		v.resize(n);
+		forn(i,n) v[i]=i;
+		rank.resize(n);
+		forn(i,n) rank[i]=1;
+	}
+	int find(int i) {
+		if(v[i]==i) return i;
+		return v[i]=this->find(v[i]);
+	}
+	void union_set(int i, int j){
+		i=this->find(i);
+		j=this->find(j);
+		if(i==j) return;
+		if(rank[i]>rank[j]) v[j]=i;
+		else v[i]=j,++rank[j];
+	}
+};
+
+ll add(ll x, ll y) {
+	x+=y;
+	while(x>=MOD) x-=MOD;
+	while(x<0) x+=MOD;
+	return x;
+}
+
+ll sub(ll x, ll y) {
+	return add(x, -y);
+}
+
+ll mul(ll x, ll y) {
+	return (x*y)%MOD;
+}
+
+ll binpow(ll x, ll y) {
+	ll z=1;
+	while(y){
+		if(y&1) z=mul(z,x);
+		x=mul(x,x);
+		y>>=1;
+	}
+	return z;
+}
+
+ll inv(ll x) {
+	return binpow(x, MOD - 2) ;
+}
+
+bool prime(ll x) {
+	for(ll i=2;i*i<=x;++i)
+		if(x%i==0) return false;
+	return true;
+}
+
+int t;
+string s;
+
+void solve() {
+	cin>>s;
+	double ans=0;
+	forn(i,s.size()) {
+		char c=s[i];
+		int j=0;
+		for(;(i+1)<s.size()&&isdigit(s[i+1]);++i) j=j*10+(s[i+1]-'0');
+		if(!j) j=1;
+		double mr;
+		switch(c) {
+			case 'C': mr=12.01;break;
+			case 'H': mr=1.008;break;
+			case 'O': mr=16.00;break;
+			case 'N': mr=14.01;break;
+		}
+		ans+=mr*j;
+	}
+	cout<<fixed<<setprecision(3)<<ans<<'\n';
+}
+
+int main() {
+	ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	cin>>t;
+	while(t--) solve();
+}
